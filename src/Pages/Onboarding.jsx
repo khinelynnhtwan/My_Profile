1395281a2b35abe8a4af783000be56c425c908bd
khinelynnhtwan <khinelynnhtwan@gmail.com>
2025-02-20 23:52:03 +0630

    finally, I fixed it this bug

--- src/Pages/Onboarding.jsx
+++ src/Pages/Onboarding.jsx
@@ -3,7 +3,7 @@ import { Card } from "../Component/Card";
 export const Onboarding = () => {
   return (
     <>
-      <section className="bg-[url(/src/img/bg.svg)] h-screen bg-cover">
+      <section className="bg-[url(/img/bg.svg)] h-screen bg-cover">
         <Card />
       </section>
     </>
